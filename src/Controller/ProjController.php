056226cb89e26c0e2677fab67a0daf90b01312af
oscaridberg <idberg.oscar@gmail.com>
2022-11-24 01:14:39 +0100

    add poker game class

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -34,7 +34,7 @@ class ProjController extends AbstractController
     {
         $deck = $session->set("pokerdeck", new \App\Card\Deck(1, 13));
 
-        return $this->render('proj/about.html.twig');
+        return $this->render('proj/pokerhome.html.twig');
     }
 
     /**
@@ -45,7 +45,49 @@ class ProjController extends AbstractController
         $deck = $session->get("pokerdeck") ?? new \App\Card\Deck(1, 13);
         $deck->shuffleDeck();
 
-        return $this->render('proj/about.html.twig');
+        $game = $session->set("pokergame", new \App\Card\Poker(2, 0, $deck));
+        $game = $session->get("pokergame");
+
+        $data = [
+            'title' => 'Five-Card-Poker',
+            'players' => $game->getPlayers(),
+            'length' => $deck->deckLength(),
+        ];
+
+        return $this->render('proj/game.html.twig', $data);
+
+        // return $this->render('proj/about.html.twig');
     
+    }
+
+    /**
+     * @Route("/proj/poker/game/draw", name="drawpoker")
+     * method={"GET"}
+     */
+    public function draw(SessionInterface $session): Response
+    {
+        $this->resetDeck($session);
+        $deck = $session->get("pokerdeck");
+        $game = $session->get("pokergame");
+        $game->drawCard($session, 1, $deck);
+        $game->drawCard($session, 0, $deck);
+        $data = [
+            'title' => 'Five-Card-Poker',
+            'players' => $game->getPlayers(),
+            'length' => $deck->deckLength(),
+            'score' => $game->getPlayerScore(1),
+            'isStarted' => $game->isStarted()
+        ];
+
+
+        return $this->render('proj/game.html.twig', $data);
+    }
+
+    public function resetDeck(SessionInterface $session)
+    {
+        $deck = $session->get("pokerdeck");
+        if ($deck->deckLength() <= 5) {
+            $session->set("pokerdeck", new \App\Card\Deck(1, 13));
+        }
     }
 }
---
+++ src/Proj/Poker.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\Player;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class Poker extends Game
+{
+    protected bool $isStarted = false;
+
+    public function drawCard(SessionInterface $session, int $player, object $deck): int
+    {
+        $hand = $this->players[$player]->getHandObject();
+      
+        $hand->addCards(5, $deck);
+
+        return $hand->sumCards();
+    }
+
+    public function checkScore(int $player, object $deck): bool
+    {
+        $playerHand = $this->players[$player]->getHandObject();
+
+        // return $playerHand->sumCards() < $this->maxScore;
+        return true;
+    }
+
+    public function getPlayerScore(int $player): int
+    {
+        return $this->players[$player]->getHandObject()->sumCards();
+    }
+
+    public function dealer(int $player, object $deck): int
+    {
+        $dealer = $this->players[$player];
+        $dealerHand = $dealer->getHandObject();
+
+        while ($dealerHand->sumCards() <= 17) {
+            $dealerHand->addCards(1, $deck);
+        }
+
+        return $dealerHand->sumCards();
+    }
+
+    public function decideWinner(): string
+    {
+        $dealer = $this->getPlayerScore(0);
+        $player = $this->getPlayerScore(1);
+        $winner = '';
+        $maxScore = 21;
+
+        if ($player > $maxScore or $player === $dealer or $dealer > $player and $dealer <= $maxScore) {
+            $winner = 'Dealer won.';
+        } elseif ($dealer > $maxScore and $player <= $maxScore or $player > $dealer) {
+            $winner = 'You won!';
+        }
+
+        return $winner;
+    }
+
+    public function isStarted(): bool
+    {
+        return $this->isStarted;
+    }
+}
